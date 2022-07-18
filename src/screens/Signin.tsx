08cfc0541dbdb77dabb086b340cb82a8223739d5
Wellington <wellingtoncarvalho11@gmail.com>
2022-07-18 00:43:20 -0300

    feature: init the project

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,9 @@
+import {VStack, Text} from 'native-base';
+
+export function SignIn(){
+    return(
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            
+        </VStack>
+    )
+}
